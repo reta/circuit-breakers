8edd79ec382a3a56250589c5cb76ffab66d10d10
reta <drreta@gmail.com>
2016-06-21 22:10:53 -0400

    Simplified Hystrix metrics event stream (using Spring Boot Actuator)

--- hystrix-spring/src/main/java/com/example/GeoIpApplication.java
+++ hystrix-spring/src/main/java/com/example/GeoIpApplication.java
@@ -6,21 +6,19 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import com.netflix.hystrix.contrib.servopublisher.HystrixServoMetricsPublisher;
 import com.netflix.hystrix.strategy.HystrixPlugins;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableHystrix
 @EnableScheduling
 public class GeoIpApplication {
 	@Bean
@@ -37,12 +35,6 @@ public class GeoIpApplication {
 	public void init() {
 		HystrixPlugins.getInstance().registerMetricsPublisher(
 			HystrixServoMetricsPublisher.getInstance());
-	}
-
-	@Bean
-	ServletRegistrationBean hystrixMetricsStreamServlet() {
-		return new ServletRegistrationBean(new HystrixMetricsStreamServlet(),
-			"/hystrix.stream");
 	}
 
 	public static void main(String[] args) throws IOException {
